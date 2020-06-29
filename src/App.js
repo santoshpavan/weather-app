bdc85b625fb0fa747fdd736aa9086f5e53722f67
santoshpavan <santoshpavan.psp@gmail.com>
2020-06-29 18:37:47 -0400

    index page made dynamic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const path = require('path');
 
+console.log(__dirname);
+
 const public_path = path.join(__dirname, "../public");
 
 const app = express();
 
-app.use(express.static(public_path));
+app.set('view engine', 'hbs');
+app.use('/public', express.static(public_path));
 
-//req -> request; res -> result
-// app.get('', (req, res) => {
-//     res.send("<h1>Root page</h1>");
-// })
+// req -> request; res -> result
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather Application',
+        description: 'This is a weather application that shows real time weather data using mapbox and weatherstack.'
+    });
+})
 
 // app.get('/help', (req, res) => {
 //     res.send("<h1>Help</h1>");
